8f2afeca92d007f6006a8a4472da6f0be7240002
Slava-tlt <cjiabug1@mail.com>
2022-06-27 19:07:28 +0400

    commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,32 @@
 package app.model;
 
+import app.model.Animal;
+import app.model.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
     private Animal animal;
+    private Timer time;
+
+    @Autowired
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer time) {
+        this.animal = animal;
+        this.time = time;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return time;
     }
 }
